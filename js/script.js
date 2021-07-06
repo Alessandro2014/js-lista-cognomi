35a8565fba57641347d09002d84944f99fc9aa91
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-06 17:03:20 +0200

    aggiunta fmyFunction

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@
 4. scrivere posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 5. stampare tutto in pagina */
 
-var list = ["Bianchi", "Neri", "Rossi", "Gialli"];
+var printName = document.getElementById("last-name-list");
 
-var userName = prompt("inserisci il tuo cognome");
+var list = ["Bianchi", "Neri", "Rossi", "Gialli" ];
+    list.indexOf(nameList);
+var nameList = " ";
+var userName = prompt("inserisci il tuo cognome").toLowerCase();
     list.push(userName);
     console.table(list);
+
+
+var i = 0;
+while (i <= list.length) {
+    nameList += "<li>" + list[i] + "</li>";
+    i++;
+}  
+
+function myFunction() {
+    nameList.sort();
+    nameList.toLowerCase();
+  }
+  printName.innerHTML = nameList;
+
